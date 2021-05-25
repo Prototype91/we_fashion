aa19e7dde2c946662d5e1cff893319b530b36c4f
Dylan Lecomte <lecomtedylan@rocketmail.com>
2021-05-25 17:47:03 +0200

    details page added

--- resources/views/front/show.blade.php
+++ resources/views/front/show.blade.php
@@ -1,7 +1,31 @@
 @extends('layouts.master')
 
 @section('content')
-<div style="width: 80%; margin: 0 auto; text-align: center">
-    <h1>{{$product->name}}</h1>
+<div class="details-ctn">
+    <div class="cart-ctn">
+        <div class="details-img">
+            <img src="https://content.asos-media.com/-/media/images/articles/men/2019/02/22-fri/how-asos-does-new-season-denim/mw-asos-style-feed-staff-style-denim-01.jpg?h=1100&w=870&la=fr-FR&hash=7B8220F6CF8523ADAC864F06AF84411B">
+        </div>
+        <div>
+            <h2>{{$product->name}}</h2>
+            <p>Référence : {{$product->ref}}</p>
+            <p>Prix TTC : {{$product->price}} €</p>
+            <form action="">
+                <label for="cart">Ajouter :</label>
+                <select id="cart">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+                <div>
+                    <button type="submit">Acheter</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="description">
+        <p>Description : {{$product->description}}</p>
+    </div>
 </div>
+
 @endsection
