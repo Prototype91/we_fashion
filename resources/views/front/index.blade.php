cdbe44e080bd78f0417b0a79417389b40b1f292f
Dylan Lecomte <lecomtedylan@rocketmail.com>
2021-05-28 12:32:11 +0200

    you can click on all the card to display details of products

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -11,31 +11,33 @@
 </div>
 <div class="list-group">
     @forelse($products as $product)
-    <div class="card">
-        <div class="img-ctn">
-        @if($product->picture)
-            <img src="{{asset('images/'.$product->picture->link)}}">
-        @else
-        <img src="https://assets.hermes.com/is/image/hermesedito/P_11_JOAILLERIE_CAVALIERE_1?fmt=webp&fit=wrap,0&wid=696">
-        @endif
-        </div>
-        <div class="container">
-            <h4><a href="{{url('product', $product->id)}}">{{$product->name}}</a></h4>
-            <p><strong>Description : </strong>{{$product->description}}</p>
-            @if($product->category)
-            <p><strong>Catégorie : </strong>{{$product->category->gender}}</p>
-            @endif
-            <p>
-                <strong>Prix : </strong>{{$product->price}} €
-                @if($product->discount)
-                <span class="new-price"> => {{round($product->price *0.5, 2)}} €</span>
+    <a href="{{url('product', $product->id)}}">
+        <div class="card">
+            <div class="img-ctn">
+                @if($product->picture)
+                <img src="{{asset('images/'.$product->picture->link)}}">
+                @else
+                <img src="https://assets.hermes.com/is/image/hermesedito/P_11_JOAILLERIE_CAVALIERE_1?fmt=webp&fit=wrap,0&wid=696">
                 @endif
-            </p>
-            <p><strong>Référence : </strong>{{$product->ref}}</p>
+            </div>
+            <div class="container">
+                <h4><a href="{{url('product', $product->id)}}">{{$product->name}}</a></h4>
+                <p><strong>Description : </strong>{{$product->description}}</p>
+                @if($product->category)
+                <p><strong>Catégorie : </strong>{{$product->category->gender}}</p>
+                @endif
+                <p>
+                    <strong>Prix : </strong>{{$product->price}} €
+                    @if($product->discount)
+                    <span class="new-price"> => {{round($product->price *0.5, 2)}} €</span>
+                    @endif
+                </p>
+                <p><strong>Référence : </strong>{{$product->ref}}</p>
+            </div>
         </div>
-    </div>
+    </a>
     @empty
-    <h2 class="empty width">Désolé, aucun article à afficher...</p>
-        @endforelse
+    <h2 class="empty width">Désolé, aucun article à afficher...</h2>
+    @endforelse
 </div>
 @endsection
